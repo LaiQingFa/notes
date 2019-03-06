327581825eed17e1bfe3b00dd3e06f52bd298b98
laiqingfa <2511695066@qq.com>
2019-03-06 20:15:34 +0800

    12

--- C/notes-C语言程序设计（第四版）谭浩强/day01/程序设计和c语言/helloword.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{ 
-	printf("hello world!\n");
-	getchar();   //此处避免执行完程序自动退出 
-
-	return 0;
-}
--- C/notes-C语言程序设计（第四版）谭浩强/day03/函数/将外部变量的作用域扩展到其他文件/2.c
+++
@@ -1,8 +0,0 @@
-extern int A;
-int power(int n)
-{
-	int i,y=1;
-	for(i=0;i<=n;i++)
-	    y*=A;
-	return(y);
-}
